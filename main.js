7e5d66a112b51c0838b85bf4b1e6fc9cd3c46a03
chungheon <36347616+chungheon@users.noreply.github.com>
2020-03-04 08:02:23 +0800

    Container for Images
    Add responsive containers to contain images

--- main.js
+++ main.js
@@ -1,49 +1,57 @@
-function setStyles(){
+function setStyles() {
   var e = document.getElementsByClassName("path");
-  for(var c of e){
+  for (var c of e) {
     c.style.transition = "0.4s";
     c.style.paddingTop = "25px";
     c.style.paddingBottom = "25px";
   }
   var navBar = document.getElementById("navbar");
   navBar.style.background = "rgba(0, 0, 0, 0)";
-  
+
+
   addAnim();
 }
 
-window.onscroll = function() {scrollFunction()};
+window.onscroll = function () { scrollFunction() };
 
 function scrollFunction() {
-  var e = document.getElementsByClassName("path");
+  var header = document.getElementById("about");
   var navBar = document.getElementById("navbar");
 
-  if(document.documentElement.scrollTop > 700){
+  // use greater than or equal to be sure.
+  if (document.documentElement.scrollTop > header.scrollHeight - 50) {
     navBar.style.background = "rgba(0, 0, 0, 0.5)";
   }else{
     navBar.style.background = "rgba(0, 0, 0, 0)";
   }
 
+  var element = document.getElementsByClassName("path");
+
   if (document.documentElement.scrollTop > 50) {
-    for(var c of e){
+    for (var c of element) {
       c.style.paddingTop = "15px";
       c.style.paddingBottom = "15px";
     }
   } else {
-    for(var c of e){
+    for (var c of element) {
       c.style.paddingTop = "25px";
       c.style.paddingBottom = "25px";
     }
   }
 }
 
-function addAnim(){
-  var elements = document.getElementsByClassName("msg");
-  for(var e of elements){
-    e.onmouseenter = function(){
-      e.classList.add('msganim');
-    }
-    e.onmouseleave = function(){
-      e.classList.remove('msganim');
-    }
+function addAnim() {
+  var e = document.getElementById("about");
+  var msg = document.getElementById("msg");
+  msg.onmouseenter = function () {
+    e.style.background = "black";
   }
+
+  msg.onmouseleave = function () {
+    e.style.background = "url(./resources/about_bg.jpg)";
+    e.style.backgroundRepeat = "no-repeat";
+    e.style.backgroundPosition = "center";
+    e.style.backgroundSize = "cover";
+  }
+
 }
